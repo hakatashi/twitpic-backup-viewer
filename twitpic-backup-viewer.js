b2239808f976cb722e217b236f36e43a66c83c2f
hakatashi <hakatasiloving@gmail.com>
2014-10-17 06:35:06 +0900

    Add initial functions

---
+++ twitpic-backup-viewer.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var morgan = require('morgan');
+var jade = require('jade');
+var less = require('less-middleware');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
+app.use(morgan('combined'));
+app.use(favicon(__dirname + '/public/favicon.ico'));
+app.use(less(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', function (req, res, next) {
+	res.render('index', {});
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+
+// error handlers
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function (err, req, res, next) {
+	res.status(err.status || 500);
+	res.render('error', {
+		message: err.message,
+		error: err
+	});
+});
+
+var server = app.listen(10723, function() {
+	console.log('Express server listening on port ' + server.address().port);
+});
